7909e5e5834dfb7ed35fddebf5dfda01cd849712
Emilio Wong <emilews.ew@gmail.com>
2019-03-05 20:49:03 -0700

    Added redux and a login screen

--- react/us/src/Footer.js
+++ react/us/src/Footer.js
@@ -1,21 +1,10 @@
 import React from "react";
-import { MDBCol, MDBContainer, MDBRow, MDBFooter } from "mdbreact";
+import { MDBContainer, MDBFooter } from "mdbreact";
 
 const FooterPage = () => {
   return (
     <div className="fixed-bottom">
-    <MDBFooter color="grey" className="font-small pt-4 mt-4">
-      <MDBContainer fluid className="text-center text-md-left">
-        <MDBRow>
-          <MDBCol md="4">
-            <h5 className="title centered">Universidad de Sonora</h5>
-            <p>
-              Here you can use rows and columns here to organize your footer
-              content.
-            </p>
-          </MDBCol>
-        </MDBRow>
-      </MDBContainer>
+    <MDBFooter color="grey" className="font-small">
       <div className="footer-copyright text-center py-3">
         <MDBContainer fluid>
           &copy; {new Date().getFullYear()} Copyright: <a href="http://www.unison.mx"> Universidad de Sonora </a>
--- react/us/src/Login.js
+++ react/us/src/Login.js
@@ -1,14 +1,45 @@
 import React, { Component } from 'react';
 import './Login.css';
 import Logo from './img/logo-unison-png-2.png'
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+
+
 
 class Login extends Component{
     render(){
         return (
             <div>
-                
                 <div>
                     <img src={Logo} alt="Logo Unison" className="Logo"/>
+                </div>
+                <div className="FormContainer">
+                    <TextField
+                        id="outlined-email-input"
+                        label="Usuario"
+                        className="textField Form"
+                        type="email"
+                        name="email"
+                        autoComplete="email"
+                        margin="normal"
+                        variant="outlined"
+                    />
+                    </div>
+                    <div className="FormContainer">
+                    <TextField
+                        id="outlined-password-input"
+                        label="Contraseña"
+                        className="textField Form"
+                        type="password"
+                        autoComplete="current-password"
+                        margin="normal"
+                        variant="outlined"
+                    />
+                </div>
+                <div className="FormContainer">
+                    <Button variant="contained" color="primary" className="button">
+                        Iniciar Sesión
+                    </Button>
                 </div>
             </div>
         )
